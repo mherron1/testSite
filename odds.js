6122f9abd715f735a00263c26481ca898a45baba
Michael Herron <mherron2021@gmail.com>
2021-07-06 13:27:35 +0100

    update

--- odds.js
+++ odds.js
@@ -1,308 +1,194 @@
 let odds = [
 { name: 'Conor McGregor', odds: '2.00' }
 ,
-{ name: 'Dustin Poirier', odds: '1.83' }
+{ name: 'Dustin Poirier', odds: '1.82' }
 ,
-{ name: 'Gilbert Burns', odds: '2.30' }
+{ name: 'Gilbert Burns', odds: '2.36' }
 ,
-{ name: 'Stephen Thompson', odds: '1.65' }
+{ name: 'Stephen Thompson', odds: '1.60' }
 ,
-{ name: 'Kris Moutinho', odds: '7.00' }
+{ name: 'Greg Hardy', odds: '2.16' }
 ,
-{ name: 'Sean Omalley', odds: '1.11' }
+{ name: 'Tai Tuivasa', odds: '1.70' }
 ,
-{ name: 'Greg Hardy', odds: '2.20' }
-,
-{ name: 'Tai Tuivasa', odds: '1.69' }
-,
-{ name: 'Irene Aldana', odds: '1.83' }
+{ name: 'Irene Aldana', odds: '1.82' }
 ,
 { name: 'Yana Kunitskaya', odds: '2.00' }
 ,
-{ name: 'Carlos Condit', odds: '2.65' }
+{ name: 'Kris Moutinho', odds: '6.90' }
 ,
-{ name: 'Max Griffin', odds: '1.50' }
+{ name: 'Sean Omalley', odds: '1.11' }
 ,
-{ name: 'Jennifer Maia', odds: '1.53' }
+{ name: 'Carlos Condit', odds: '2.52' }
 ,
-{ name: 'Jessica Eye', odds: '2.60' }
+{ name: 'Max Griffin', odds: '1.53' }
+,
+{ name: 'Michel Pereira', odds: '1.60' }
+,
+{ name: 'Niko Price', odds: '2.36' }
 ,
 { name: 'Ilia Topuria', odds: '1.40' }
 ,
-{ name: 'Ryan Hall', odds: '3.00' }
-,
-{ name: 'Michel Pereira', odds: '1.63' }
-,
-{ name: 'Niko Price', odds: '2.35' }
+{ name: 'Ryan Hall', odds: '2.98' }
 ,
 { name: 'Dricus Du Plessis', odds: '1.87' }
 ,
 { name: 'Trevin Giles', odds: '1.95' }
 ,
-{ name: 'Brad Tavares', odds: '1.67' }
+{ name: 'Jennifer Maia', odds: '1.50' }
 ,
-{ name: 'Omari Akhmedov', odds: '2.25' }
+{ name: 'Jessica Eye', odds: '2.60' }
+,
+{ name: 'Brad Tavares', odds: '1.57' }
+,
+{ name: 'Omari Akhmedov', odds: '2.42' }
 ,
 { name: 'Jerome Rivera', odds: '3.90' }
 ,
-{ name: 'Zhalgas Zhumagulov', odds: '1.27' }
+{ name: 'Zhalgas Zhumagulov', odds: '1.26' }
 ,
-{ name: 'Alen Amedovski', odds: '1.67' }
+{ name: 'Alen Amedovski', odds: '1.66' }
 ,
-{ name: 'Hu Yaozong', odds: '2.25' }
+{ name: 'Hu Yaozong', odds: '2.24' }
 ,
-{ name: 'Dustin Stoltzfus', odds: '2.90' }
+{ name: 'Dustin Stoltzfus', odds: '2.98' }
 ,
-{ name: 'Rodolfo Vieira', odds: '1.44' }
+{ name: 'Rodolfo Vieira', odds: '1.41' }
 ,
-{ name: 'Alan Baudot', odds: '3.75' }
+{ name: 'Amir Albazi', odds: '1.41' }
 ,
-{ name: 'Rodrigo Nascimento', odds: '1.29' }
+{ name: 'Ode Osbourne', odds: '2.98' }
 ,
-{ name: 'Marion Reneau', odds: '2.20' }
+{ name: 'Don Madge', odds: '2.60' }
 ,
-{ name: 'Miesha Tate', odds: '1.69' }
+{ name: 'Guram Kutateladze', odds: '1.52' }
 ,
-{ name: 'Islam Makhachev', odds: '1.15' }
+{ name: 'Alan Baudot', odds: '3.45' }
 ,
-{ name: 'Thiago Moises', odds: '5.50' }
+{ name: 'Rodrigo Nascimento', odds: '1.32' }
 ,
-{ name: 'Jeremy Stephens', odds: '3.00' }
+{ name: 'Billy Quarantillo', odds: '2.76' }
 ,
-{ name: 'Mateusz Gamrot', odds: '1.40' }
+{ name: 'Gabriel Benitez', odds: '1.47' }
 ,
-{ name: 'Abubakar Nurmagomedov', odds: '2.00' }
+{ name: 'Marion Reneau', odds: '2.08' }
 ,
-{ name: 'Daniel Rodriguez', odds: '1.83' }
+{ name: 'Miesha Tate', odds: '1.77' }
 ,
-{ name: 'Khalid Taha', odds: '1.57' }
+{ name: 'Jeremy Stephens', odds: '2.98' }
 ,
-{ name: 'Sergey Morozov', odds: '2.45' }
+{ name: 'Mateusz Gamrot', odds: '1.41' }
 ,
-{ name: 'Deron Winn', odds: '3.20' }
+{ name: 'Abubakar Nurmagomedov', odds: '1.96' }
 ,
-{ name: 'Phil Hawes', odds: '1.36' }
+{ name: 'Daniel Rodriguez', odds: '1.87' }
 ,
-{ name: 'Amanda Lemos', odds: '1.20' }
+{ name: 'Deron Winn', odds: '3.10' }
 ,
-{ name: 'Montserrat Ruiz', odds: '4.75' }
+{ name: 'Phil Hawes', odds: '1.38' }
 ,
-{ name: 'Anderson Dos Santos', odds: '2.45' }
+{ name: 'Amanda Lemos', odds: '1.19' }
 ,
-{ name: 'Miles Johns', odds: '1.57' }
+{ name: 'Montserrat Ruiz', odds: '4.85' }
 ,
-{ name: 'Francisco Figueredo', odds: '1.33' }
+{ name: 'Khalid Taha', odds: '1.63' }
 ,
-{ name: 'Malcolm Gordon', odds: '3.50' }
+{ name: 'Sergey Morozov', odds: '2.32' }
 ,
-{ name: 'Cory Sandhagen', odds: '1.57' }
+{ name: 'Anderson Dos Santos', odds: '2.55' }
 ,
-{ name: 'T.J. Dillashaw', odds: '2.45' }
+{ name: 'Miles Johns', odds: '1.53' }
 ,
-{ name: 'Aspen Ladd', odds: '1.63' }
+{ name: 'Francisco Figueredo', odds: '1.32' }
 ,
-{ name: 'Macy Chiasson', odds: '2.30' }
+{ name: 'Malcolm Gordon', odds: '3.45' }
 ,
-{ name: 'Adrian Yanez', odds: '1.61' }
+{ name: 'Cory Sandhagen', odds: '1.61' }
 ,
-{ name: 'Randy Costa', odds: '2.40' }
+{ name: 'T.J. Dillashaw', odds: '2.36' }
 ,
-{ name: 'Brendan Allen', odds: '2.15' }
+{ name: 'Aspen Ladd', odds: '1.60' }
 ,
-{ name: 'Puna Soriano', odds: '1.74' }
+{ name: 'Macy Chiasson', odds: '2.38' }
+,
+{ name: 'Adrian Yanez', odds: '1.54' }
+,
+{ name: 'Randy Costa', odds: '2.52' }
+,
+{ name: 'Kyler Phillips', odds: '1.47' }
+,
+{ name: 'Raulian Paiva', odds: '2.76' }
+,
+{ name: 'Tony Kelley', odds: '2.12' }
+,
+{ name: 'Trevin Jones', odds: '1.75' }
+,
+{ name: 'Jordan Williams', odds: '1.47' }
+,
+{ name: 'Mickey Gall', odds: '2.76' }
+,
+{ name: 'Ian Heinisch', odds: '1.72' }
+,
+{ name: 'Nassourdine Imavov', odds: '2.15' }
+,
+{ name: 'Andre Ewell', odds: '2.55' }
+,
+{ name: 'Julio Arce', odds: '1.53' }
+,
+{ name: 'Darren Elkins', odds: '2.25' }
+,
+{ name: 'Darrick Minner', odds: '1.67' }
+,
+{ name: 'Elise Reed', odds: '2.95' }
+,
+{ name: 'Sijara Eubanks', odds: '1.42' }
+,
+{ name: 'Maycee Barber', odds: '2.40' }
+,
+{ name: 'Miranda Maverick', odds: '1.59' }
+,
+{ name: 'Brendan Allen', odds: '1.91' }
+,
+{ name: 'Puna Soriano', odds: '1.91' }
 ,
 { name: 'Chris Daukaus', odds: '1.50' }
 ,
-{ name: 'Shamil Abdurakhimov', odds: '2.65' }
+{ name: 'Shamil Abdurakhimov', odds: '2.64' }
 ,
-{ name: 'Kyler Phillips', odds: '1.44' }
+{ name: 'Aaron Phillips', odds: '1.58' }
 ,
-{ name: 'Raulian Paiva', odds: '2.80' }
+{ name: 'Cameron Else', odds: '2.42' }
 ,
-{ name: 'Tony Kelley', odds: '2.10' }
+{ name: 'A.J. McKee', odds: '2.00' }
 ,
-{ name: 'Trevin Jones', odds: '1.74' }
-,
-{ name: 'Jordan Williams', odds: '1.53' }
-,
-{ name: 'Mickey Gall', odds: '2.55' }
-,
-{ name: 'Ian Heinisch', odds: '1.69' }
-,
-{ name: 'Nassourdine Imavov', odds: '2.20' }
-,
-{ name: 'Andre Ewell', odds: '2.50' }
-,
-{ name: 'Julio Arce', odds: '1.54' }
-,
-{ name: 'Darren Elkins', odds: '2.10' }
-,
-{ name: 'Darrick Minner', odds: '1.74' }
-,
-{ name: 'Maycee Barber', odds: '2.35' }
-,
-{ name: 'Miranda Maverick', odds: '1.63' }
-,
-{ name: 'Last change: 2 min ago', odds: '1.48' }
-,
-{ name: 'BELLATORJuly 31st', odds: '2.15' }
-,
-{ name: 'A.J. McKee', odds: '2.05' }
-,
-{ name: 'Patricky Freire', odds: '1.80' }
-,
-{ name: 'Last change: 12 hr 9 min ago', odds: '1.63' }
-,
-{ name: 'UFC 265: LEWIS VS. GANEAugust 7th', odds: '2.40' }
-,
-{ name: 'Amanda Nunes', odds: '1.13' }
-,
-{ name: 'Julianna Pena', odds: '6.25' }
+{ name: 'Patricky Freire', odds: '1.82' }
 ,
 { name: 'Ciryl Gane', odds: '1.36' }
 ,
 { name: 'Derrick Lewis', odds: '3.20' }
 ,
-{ name: 'Jose Aldo', odds: '1.71' }
+{ name: 'Amanda Nunes', odds: '1.13' }
+,
+{ name: 'Julianna Pena', odds: '6.40' }
+,
+{ name: 'Jose Aldo', odds: '1.72' }
 ,
 { name: 'Pedro Munhoz', odds: '2.15' }
 ,
-{ name: 'Last change: 1 day 13 hr ago', odds: '1.67' }
-,
-{ name: 'Darren Till', odds: '1.71' }
+{ name: 'Darren Till', odds: '1.72' }
 ,
 { name: 'Derek Brunson', odds: '2.15' }
 ,
-{ name: 'Jared Cannonier', odds: '1.40' }
+{ name: 'Jared Cannonier', odds: '1.42' }
 ,
 { name: 'Kelvin Gastelum', odds: '2.95' }
 ,
-{ name: 'Johnny Walker', odds: '2.30' }
+{ name: 'Johnny Walker', odds: '2.32' }
 ,
 { name: 'Thiago Santos', odds: '1.63' }
 ,
-{ name: 'Glover Teixeira', odds: '3.00' }
+{ name: 'Glover Teixeira', odds: '3.10' }
 ,
-{ name: 'Jan Blachowicz', odds: '1.40' }
-,
-{ name: 'Alexander Volkov', odds: '2.40' }
-,
-{ name: 'Ciryl Gane', odds: '1.63' }
-,
-{ name: 'Michel Prazeres', odds: '3.10' }
-,
-{ name: 'Shavkat Rakhmonov', odds: '1.40' }
-,
-{ name: 'Maxim Grishin', odds: '1.95' }
-,
-{ name: 'Ovince St. Preux', odds: '1.87' }
-,
-{ name: 'Charles Rosa', odds: '1.61' }
-,
-{ name: 'Justin Jaynes', odds: '2.45' }
-,
-{ name: 'Raoni Barcelos', odds: '1.48' }
-,
-{ name: 'Timur Valiev', odds: '2.75' }
-,
-{ name: 'Nicolas Dalby', odds: '1.40' }
-,
-{ name: 'Sergey Khandozhko', odds: '3.10' }
-,
-{ name: 'Ramazan Emeev', odds: '1.69' }
-,
-{ name: 'Warlley Alves', odds: '2.25' }
-,
-{ name: 'Jai Herbert', odds: '3.05' }
-,
-{ name: 'Renato Moicano', odds: '1.41' }
-,
-{ name: 'Isaac Villanueva', odds: '2.80' }
-,
-{ name: 'Marcin Prachnio', odds: '1.45' }
-,
-{ name: 'Damir Hadzovic', odds: '1.69' }
-,
-{ name: 'Yancy Medeiros', odds: '2.25' }
-,
-{ name: 'Su Mudaerji', odds: '1.69' }
-,
-{ name: 'Tim Elliott', odds: '2.25' }
-,
-{ name: 'Conor McGregor', odds: '2.00' }
-,
-{ name: 'Dustin Poirier', odds: '1.83' }
-,
-{ name: 'Ilia Topuria', odds: '1.34' }
-,
-{ name: 'Ryan Hall', odds: '3.35' }
-,
-{ name: 'Louis Smolka', odds: '4.20' }
-,
-{ name: "Sean O'Malley", odds: '1.24' }
-,
-{ name: 'Kevin Lee', odds: '2.55' }
-,
-{ name: 'Sean Brady', odds: '1.54' }
-,
-{ name: 'Irene Aldana', odds: '1.65' }
-,
-{ name: 'Yana Kunitskaya', odds: '2.25' }
-,
-{ name: 'Gilbert Burns', odds: '2.10' }
-,
-{ name: 'Stephen Thompson', odds: '1.77' }
-,
-{ name: 'Carlos Condit', odds: '2.40' }
-,
-{ name: 'Max Griffin', odds: '1.59' }
-,
-{ name: 'Michel Pereira', odds: '1.61' }
-,
-{ name: 'Niko Price', odds: '2.35' }
-,
-{ name: 'Jerome Rivera', odds: '4.15' }
-,
-{ name: 'Zhalgas Zhumagulov', odds: '1.24' }
-,
-{ name: 'Dricus Du Plessis', odds: '1.83' }
-,
-{ name: 'Trevin Giles', odds: '1.91' }
-,
-{ name: 'Alen Amedovski', odds: '1.54' }
-,
-{ name: 'Hu Yaozong', odds: '2.55' }
-,
-{ name: 'Jennifer Maia', odds: '1.53' }
-,
-{ name: 'Jessica Eye', odds: '2.65' }
-,
-{ name: 'Greg Hardy', odds: '2.20' }
-,
-{ name: 'Tai Tuivasa', odds: '1.67' }
-,
-{ name: 'Brad Tavares', odds: '1.56' }
-,
-{ name: 'Omari Akhmedov', odds: '2.50' }
-,
-{ name: 'Max Holloway', odds: '1.23' }
-,
-{ name: 'Yair Rodriguez', odds: '4.25' }
-,
-{ name: 'Alan Baudot', odds: '3.75' }
-,
-{ name: 'Rodrigo Nascimento', odds: '1.27' }
-,
-{ name: 'Maycee Barber', odds: '2.30' }
-,
-{ name: 'Miranda Maverick', odds: '1.67' }
-,
-{ name: 'Amanda Nunes', odds: '1.13' }
-,
-{ name: 'Julianna Pena', odds: '6.25' }
-,
-{ name: 'Glover Teixeira', odds: '2.90' }
-,
-{ name: 'Jan Blachowicz', odds: '1.40' }
+{ name: 'Jan Blachowicz', odds: '1.38' }
 ,
 ]
--- oddsData.js
+++ oddsData.js
@@ -5,55 +5,55 @@ let obj = [
   },
   {
     row1: "Dustin Poirier",
-    row2: "-120",
+    row2: "-122",
   },
   {
     row1: "Gilbert Burns",
-    row2: "130",
+    row2: "136",
   },
   {
     row1: "Stephen Thompson",
-    row2: "-155",
-  },
-  {
-    row1: "Kris Moutinho",
-    row2: "600",
-  },
-  {
-    row1: "Sean Omalley",
-    row2: "-900",
+    row2: "-168",
   },
   {
     row1: "Greg Hardy",
-    row2: "120",
+    row2: "116",
   },
   {
     row1: "Tai Tuivasa",
-    row2: "-145",
+    row2: "-142",
   },
   {
     row1: "Irene Aldana",
-    row2: "-120",
+    row2: "-122",
   },
   {
     row1: "Yana Kunitskaya",
     row2: "100",
   },
   {
+    row1: "Kris Moutinho",
+    row2: "590",
+  },
+  {
+    row1: "Sean Omalley",
+    row2: "-950",
+  },
+  {
     row1: "Carlos Condit",
-    row2: "165",
+    row2: "152",
   },
   {
     row1: "Max Griffin",
-    row2: "-200",
+    row2: "-188",
   },
   {
-    row1: "Jennifer Maia",
-    row2: "-190",
+    row1: "Michel Pereira",
+    row2: "-168",
   },
   {
-    row1: "Jessica Eye",
-    row2: "160",
+    row1: "Niko Price",
+    row2: "136",
   },
   {
     row1: "Ilia Topuria",
@@ -61,15 +61,7 @@ let obj = [
   },
   {
     row1: "Ryan Hall",
-    row2: "200",
-  },
-  {
-    row1: "Michel Pereira",
-    row2: "-160",
-  },
-  {
-    row1: "Niko Price",
-    row2: "135",
+    row2: "198",
   },
   {
     row1: "Dricus Du Plessis",
@@ -80,12 +72,20 @@ let obj = [
     row2: "-105",
   },
   {
+    row1: "Jennifer Maia",
+    row2: "-200",
+  },
+  {
+    row1: "Jessica Eye",
+    row2: "160",
+  },
+  {
     row1: "Brad Tavares",
-    row2: "-150",
+    row2: "-176",
   },
   {
     row1: "Omari Akhmedov",
-    row2: "125",
+    row2: "142",
   },
   {
     row1: "Jerome Rivera",
@@ -93,18 +93,18 @@ let obj = [
   },
   {
     row1: "Zhalgas Zhumagulov",
-    row2: "-375",
+    row2: "-385",
   },
   {
     row1: "Alen Amedovski",
-    row2: "-150",
+    row2: "-152",
   },
   {
     row1: "Hu Yaozong",
-    row2: "125",
+    row2: "124",
   },
   {
-    row1: "Last change: < 1 min ago",
+    row1: "Last change: 16 min ago",
     row2: "",
   },
   {
@@ -113,119 +113,119 @@ let obj = [
   },
   {
     row1: "",
-    row2: "BetMGM",
+    row2: "FanDuel",
   },
   {
     row1: "Dustin Stoltzfus",
-    row2: "190",
+    row2: "198",
   },
   {
     row1: "Rodolfo Vieira",
-    row2: "-225",
-  },
-  {
-    row1: "Billy Quarantillo",
-    row2: "",
-  },
-  {
-    row1: "Gabriel Benitez",
-    row2: "",
-  },
-  {
-    row1: "Alan Baudot",
-    row2: "275",
-  },
-  {
-    row1: "Rodrigo Nascimento",
-    row2: "-350",
+    row2: "-245",
   },
   {
     row1: "Amir Albazi",
-    row2: "",
+    row2: "-245",
   },
   {
     row1: "Ode Osbourne",
-    row2: "",
+    row2: "198",
   },
   {
     row1: "Don Madge",
-    row2: "",
+    row2: "160",
   },
   {
     row1: "Guram Kutateladze",
+    row2: "-194",
+  },
+  {
+    row1: "Alan Baudot",
+    row2: "245",
+  },
+  {
+    row1: "Rodrigo Nascimento",
+    row2: "-310",
+  },
+  {
+    row1: "Billy Quarantillo",
+    row2: "176",
+  },
+  {
+    row1: "Gabriel Benitez",
+    row2: "-215",
+  },
+  {
+    row1: "Islam Makhachev",
+    row2: "",
+  },
+  {
+    row1: "Thiago Moises",
     row2: "",
   },
   {
     row1: "Marion Reneau",
-    row2: "120",
+    row2: "108",
   },
   {
     row1: "Miesha Tate",
-    row2: "-145",
-  },
-  {
-    row1: "Islam Makhachev",
-    row2: "-650",
-  },
-  {
-    row1: "Thiago Moises",
-    row2: "450",
+    row2: "-130",
   },
   {
     row1: "Jeremy Stephens",
-    row2: "200",
+    row2: "198",
   },
   {
     row1: "Mateusz Gamrot",
-    row2: "-250",
+    row2: "-245",
   },
   {
     row1: "Abubakar Nurmagomedov",
-    row2: "100",
+    row2: "-104",
   },
   {
     row1: "Daniel Rodriguez",
-    row2: "-120",
-  },
-  {
-    row1: "Khalid Taha",
-    row2: "-175",
-  },
-  {
-    row1: "Sergey Morozov",
-    row2: "145",
+    row2: "-115",
   },
   {
     row1: "Deron Winn",
-    row2: "220",
+    row2: "210",
   },
   {
     row1: "Phil Hawes",
-    row2: "-275",
+    row2: "-260",
   },
   {
     row1: "Amanda Lemos",
-    row2: "-500",
+    row2: "-520",
   },
   {
     row1: "Montserrat Ruiz",
-    row2: "375",
+    row2: "385",
+  },
+  {
+    row1: "Khalid Taha",
+    row2: "-160",
+  },
+  {
+    row1: "Sergey Morozov",
+    row2: "132",
   },
   {
     row1: "Anderson Dos Santos",
-    row2: "145",
+    row2: "155",
   },
   {
     row1: "Miles Johns",
-    row2: "-175",
+    row2: "-188",
   },
   {
     row1: "Francisco Figueredo",
-    row2: "-300",
+    row2: "-310",
   },
   {
     row1: "Malcolm Gordon",
-    row2: "250",
+    row2: "245",
   },
   {
     row1: "Last change: < 1 min ago",
@@ -237,39 +237,103 @@ let obj = [
   },
   {
     row1: "",
-    row2: "BetMGM",
+    row2: "FanDuel",
   },
   {
     row1: "Cory Sandhagen",
-    row2: "-175",
+    row2: "-164",
   },
   {
     row1: "T.J. Dillashaw",
-    row2: "145",
+    row2: "136",
   },
   {
     row1: "Aspen Ladd",
-    row2: "-160",
+    row2: "-166",
   },
   {
     row1: "Macy Chiasson",
-    row2: "130",
+    row2: "138",
   },
   {
     row1: "Adrian Yanez",
-    row2: "-165",
+    row2: "-184",
   },
   {
     row1: "Randy Costa",
-    row2: "140",
+    row2: "152",
   },
   {
-    row1: "Brendan Allen",
+    row1: "Kyler Phillips",
+    row2: "-215",
+  },
+  {
+    row1: "Raulian Paiva",
+    row2: "176",
+  },
+  {
+    row1: "Tony Kelley",
+    row2: "112",
+  },
+  {
+    row1: "Trevin Jones",
+    row2: "-134",
+  },
+  {
+    row1: "Jordan Williams",
+    row2: "-215",
+  },
+  {
+    row1: "Mickey Gall",
+    row2: "176",
+  },
+  {
+    row1: "Ian Heinisch",
+    row2: "-138",
+  },
+  {
+    row1: "Nassourdine Imavov",
     row2: "115",
   },
   {
+    row1: "Andre Ewell",
+    row2: "155",
+  },
+  {
+    row1: "Julio Arce",
+    row2: "-188",
+  },
+  {
+    row1: "Darren Elkins",
+    row2: "125",
+  },
+  {
+    row1: "Darrick Minner",
+    row2: "-150",
+  },
+  {
+    row1: "Elise Reed",
+    row2: "195",
+  },
+  {
+    row1: "Sijara Eubanks",
+    row2: "-240",
+  },
+  {
+    row1: "Maycee Barber",
+    row2: "140",
+  },
+  {
+    row1: "Miranda Maverick",
+    row2: "-170",
+  },
+  {
+    row1: "Brendan Allen",
+    row2: "-110",
+  },
+  {
     row1: "Puna Soriano",
-    row2: "-135",
+    row2: "-110",
   },
   {
     row1: "Chris Daukaus",
@@ -277,119 +341,47 @@ let obj = [
   },
   {
     row1: "Shamil Abdurakhimov",
-    row2: "165",
-  },
-  {
-    row1: "Kyler Phillips",
-    row2: "-225",
-  },
-  {
-    row1: "Raulian Paiva",
-    row2: "180",
-  },
-  {
-    row1: "Tony Kelley",
-    row2: "110",
-  },
-  {
-    row1: "Trevin Jones",
-    row2: "-135",
-  },
-  {
-    row1: "Jordan Williams",
-    row2: "-190",
-  },
-  {
-    row1: "Mickey Gall",
-    row2: "155",
+    row2: "164",
   },
   {
     row1: "Aaron Phillips",
-    row2: "",
+    row2: "-172",
   },
   {
     row1: "Cameron Else",
+    row2: "142",
+  },
+  {
+    row1: "Last change: 3 hr 58 min ago",
     row2: "",
   },
   {
-    row1: "Ian Heinisch",
-    row2: "-145",
-  },
-  {
-    row1: "Nassourdine Imavov",
-    row2: "120",
-  },
-  {
-    row1: "Andre Ewell",
-    row2: "150",
-  },
-  {
-    row1: "Julio Arce",
-    row2: "-185",
-  },
-  {
-    row1: "Darren Elkins",
-    row2: "110",
-  },
-  {
-    row1: "Darrick Minner",
-    row2: "-135",
-  },
-  {
-    row1: "Elise Reed",
+    row1: "BELLATOR 263July 31st",
     row2: "",
-  },
-  {
-    row1: "Sijara Eubanks",
-    row2: "",
-  },
-  {
-    row1: "Maycee Barber",
-    row2: "135",
-  },
-  {
-    row1: "Miranda Maverick",
-    row2: "-160",
-  },
-  {
-    row1: "Last change: 2 min ago",
-    row2: "-210",
-  },
-  {
-    row1: "BELLATORJuly 31st",
-    row2: "115",
   },
   {
     row1: "",
-    row2: "BetMGM",
+    row2: "FanDuel",
   },
   {
     row1: "A.J. McKee",
-    row2: "105",
+    row2: "100",
   },
   {
     row1: "Patricky Freire",
-    row2: "-125",
+    row2: "-122",
   },
   {
-    row1: "Last change: 12 hr 9 min ago",
-    row2: "-160",
+    row1: "Last change: 4 days ago",
+    row2: "",
   },
   {
     row1: "UFC 265: LEWIS VS. GANEAugust 7th",
-    row2: "140",
+    row2: "",
   },
   {
     row1: "",
-    row2: "BetMGM",
-  },
-  {
-    row1: "Amanda Nunes",
-    row2: "-750",
-  },
-  {
-    row1: "Julianna Pena",
-    row2: "525",
+    row2: "FanDuel",
   },
   {
     row1: "Ciryl Gane",
@@ -400,16 +392,24 @@ let obj = [
     row2: "220",
   },
   {
+    row1: "Amanda Nunes",
+    row2: "-800",
+  },
+  {
+    row1: "Julianna Pena",
+    row2: "540",
+  },
+  {
     row1: "Jose Aldo",
-    row2: "-140",
+    row2: "-138",
   },
   {
     row1: "Pedro Munhoz",
     row2: "115",
   },
   {
-    row1: "Last change: 1 day 13 hr ago",
-    row2: "-150",
+    row1: "Last change: 22 hr 34 min ago",
+    row2: "",
   },
   {
     row1: "UFC FIGHT NIGHT 191August 14th",
@@ -417,18 +417,18 @@ let obj = [
   },
   {
     row1: "",
-    row2: "BetMGM",
+    row2: "FanDuel",
   },
   {
     row1: "Darren Till",
-    row2: "-140",
+    row2: "-138",
   },
   {
     row1: "Derek Brunson",
     row2: "115",
   },
   {
-    row1: "Last change: 2 days ago",
+    row1: "Last change: 6 days ago",
     row2: "",
   },
   {
@@ -437,18 +437,18 @@ let obj = [
   },
   {
     row1: "",
-    row2: "BetMGM",
+    row2: "FanDuel",
   },
   {
     row1: "Jared Cannonier",
-    row2: "-250",
+    row2: "-240",
   },
   {
     row1: "Kelvin Gastelum",
     row2: "195",
   },
   {
-    row1: "Last change: 2 days ago",
+    row1: "Last change: 6 days ago",
     row2: "",
   },
   {
@@ -457,11 +457,11 @@ let obj = [
   },
   {
     row1: "",
-    row2: "BetMGM",
+    row2: "FanDuel",
   },
   {
     row1: "Johnny Walker",
-    row2: "130",
+    row2: "132",
   },
   {
     row1: "Thiago Santos",
@@ -476,7 +476,7 @@ let obj = [
     row2: "",
   },
   {
-    row1: "Last change: 7 hr 42 min ago",
+    row1: "Last change: 1 day 22 hr ago",
     row2: "",
   },
   {
@@ -485,458 +485,17 @@ let obj = [
   },
   {
     row1: "",
-    row2: "BetMGM",
+    row2: "FanDuel",
   },
   {
     row1: "Glover Teixeira",
-    row2: "200",
-  },
-  {
-    row1: "Jan Blachowicz",
-    row2: "-250",
-  },
-  {
-    row1: "Marta Waliczek",
-    row2: "",
-  },
-  {
-    row1: "Rizlen Zouak",
-    row2: "",
-  },
-  {
-    row1: "Bojan Velickovic",
-    row2: "",
-  },
-  {
-    row1: "Emmanuel Dawa",
-    row2: "",
-  },
-  {
-    row1: "Daniel Hromek",
-    row2: "",
-  },
-  {
-    row1: "Vaclav Holota",
-    row2: "",
-  },
-  {
-    row1: "Karol Rysavy",
-    row2: "",
-  },
-  {
-    row1: "Marek Bartl",
-    row2: "",
-  },
-  {
-    row1: "Anatoly Tarasenko",
-    row2: "",
-  },
-  {
-    row1: "Jan Fajk",
-    row2: "",
-  },
-  {
-    row1: "Last change: 11 hr 14 min ago",
-    row2: "",
-  },
-  {
-    row1: "UFC FIGHT NIGHT: GANE VS. VOLKOVJune 26th",
-    row2: "",
-  },
-  {
-    row1: "",
-    row2: "5Dimes",
-  },
-  {
-    row1: "Alexander Volkov",
-    row2: "140",
-  },
-  {
-    row1: "Ciryl Gane",
-    row2: "-160",
-  },
-  {
-    row1: "Danilo Marques",
-    row2: "",
-  },
-  {
-    row1: "Kennedy Nzechukwu",
-    row2: "",
-  },
-  {
-    row1: "Anthony Hernandez",
-    row2: "",
-  },
-  {
-    row1: "Punahele Soriano",
-    row2: "",
-  },
-  {
-    row1: "Michel Prazeres",
     row2: "210",
   },
   {
-    row1: "Shavkat Rakhmonov",
-    row2: "-250",
-  },
-  {
-    row1: "Maxim Grishin",
-    row2: "-105",
-  },
-  {
-    row1: "Ovince St. Preux",
-    row2: "-115",
-  },
-  {
-    row1: "Charles Rosa",
-    row2: "-165",
-  },
-  {
-    row1: "Justin Jaynes",
-    row2: "145",
-  },
-  {
-    row1: "Raoni Barcelos",
-    row2: "-210",
-  },
-  {
-    row1: "Timur Valiev",
-    row2: "175",
-  },
-  {
-    row1: "Nicolas Dalby",
-    row2: "-250",
-  },
-  {
-    row1: "Sergey Khandozhko",
-    row2: "210",
-  },
-  {
-    row1: "Ramazan Emeev",
-    row2: "-145",
-  },
-  {
-    row1: "Warlley Alves",
-    row2: "125",
-  },
-  {
-    row1: "Jai Herbert",
-    row2: "205",
-  },
-  {
-    row1: "Renato Moicano",
-    row2: "-245",
-  },
-  {
-    row1: "Isaac Villanueva",
-    row2: "180",
-  },
-  {
-    row1: "Marcin Prachnio",
-    row2: "-220",
-  },
-  {
-    row1: "Damir Hadzovic",
-    row2: "-145",
-  },
-  {
-    row1: "Yancy Medeiros",
-    row2: "125",
-  },
-  {
-    row1: "Su Mudaerji",
-    row2: "-145",
-  },
-  {
-    row1: "Tim Elliott",
-    row2: "125",
-  },
-  {
-    row1: "Danilo Marques",
-    row2: "",
-  },
-  {
-    row1: "Ed Herman",
-    row2: "",
-  },
-  {
-    row1: "Last change: 26 min ago",
-    row2: "",
-  },
-  {
-    row1: "UFC 264: POIRIER VS. MCGREGOR 3July 10th",
-    row2: "",
-  },
-  {
-    row1: "",
-    row2: "5Dimes",
-  },
-  {
-    row1: "Conor McGregor",
-    row2: "100",
-  },
-  {
-    row1: "Dustin Poirier",
-    row2: "-120",
-  },
-  {
-    row1: "Ilia Topuria",
-    row2: "-295",
-  },
-  {
-    row1: "Ryan Hall",
-    row2: "235",
-  },
-  {
-    row1: "Louis Smolka",
-    row2: "320",
-  },
-  {
-    row1: "Sean O'Malley",
-    row2: "-425",
-  },
-  {
-    row1: "Kevin Lee",
-    row2: "155",
-  },
-  {
-    row1: "Sean Brady",
-    row2: "-185",
-  },
-  {
-    row1: "Irene Aldana",
-    row2: "-155",
-  },
-  {
-    row1: "Yana Kunitskaya",
-    row2: "125",
-  },
-  {
-    row1: "Gilbert Burns",
-    row2: "110",
-  },
-  {
-    row1: "Stephen Thompson",
-    row2: "-130",
-  },
-  {
-    row1: "Carlos Condit",
-    row2: "140",
-  },
-  {
-    row1: "Max Griffin",
-    row2: "-170",
-  },
-  {
-    row1: "Michel Pereira",
-    row2: "-165",
-  },
-  {
-    row1: "Niko Price",
-    row2: "135",
-  },
-  {
-    row1: "Jerome Rivera",
-    row2: "315",
-  },
-  {
-    row1: "Zhalgas Zhumagulov",
-    row2: "-410",
-  },
-  {
-    row1: "Dricus Du Plessis",
-    row2: "-120",
-  },
-  {
-    row1: "Trevin Giles",
-    row2: "-110",
-  },
-  {
-    row1: "Alen Amedovski",
-    row2: "-185",
-  },
-  {
-    row1: "Hu Yaozong",
-    row2: "155",
-  },
-  {
-    row1: "Jennifer Maia",
-    row2: "-190",
-  },
-  {
-    row1: "Jessica Eye",
-    row2: "165",
-  },
-  {
-    row1: "Greg Hardy",
-    row2: "120",
-  },
-  {
-    row1: "Tai Tuivasa",
-    row2: "-150",
-  },
-  {
-    row1: "Brad Tavares",
-    row2: "-180",
-  },
-  {
-    row1: "Omari Akhmedov",
-    row2: "150",
-  },
-  {
-    row1: "Last change: 52 min ago",
-    row2: "",
-  },
-  {
-    row1: "UFC ON ESPN: HOLLOWAY VS. RODRIGUEZJuly 17th",
-    row2: "",
-  },
-  {
-    row1: "",
-    row2: "5Dimes",
-  },
-  {
-    row1: "Max Holloway",
-    row2: "-435",
-  },
-  {
-    row1: "Yair Rodriguez",
-    row2: "325",
-  },
-  {
-    row1: "Alan Baudot",
-    row2: "275",
-  },
-  {
-    row1: "Rodrigo Nascimento",
-    row2: "-365",
-  },
-  {
-    row1: "Last change: 15 hr 47 min ago",
-    row2: "",
-  },
-  {
-    row1: "UFC ON ESPN: SANDHAGEN VS. DILLASHAWJuly 24th",
-    row2: "",
-  },
-  {
-    row1: "",
-    row2: "5Dimes",
-  },
-  {
-    row1: "Cory Sandhagen",
-    row2: "",
-  },
-  {
-    row1: "T.J. Dillashaw",
-    row2: "",
-  },
-  {
-    row1: "Maycee Barber",
-    row2: "130",
-  },
-  {
-    row1: "Miranda Maverick",
-    row2: "-150",
-  },
-  {
-    row1: "Last change: 3 days ago",
-    row2: "",
-  },
-  {
-    row1: "UFC 265August 7th",
-    row2: "",
-  },
-  {
-    row1: "",
-    row2: "5Dimes",
-  },
-  {
-    row1: "Amanda Nunes",
-    row2: "-750",
-  },
-  {
-    row1: "Julianna Pena",
-    row2: "525",
-  },
-  {
-    row1: "Jose Aldo",
-    row2: "",
-  },
-  {
-    row1: "Pedro Munhoz",
-    row2: "",
-  },
-  {
-    row1: "Last change: 8 days ago",
-    row2: "",
-  },
-  {
-    row1: "UFC FIGHT NIGHT 192August 14th",
-    row2: "",
-  },
-  {
-    row1: "",
-    row2: "5Dimes",
-  },
-  {
-    row1: "Darren Till",
-    row2: "",
-  },
-  {
-    row1: "Derek Brunson",
-    row2: "",
-  },
-  {
-    row1: "Last change: 2 days ago",
-    row2: "",
-  },
-  {
-    row1: "UFC ON ESPN: CANNONIER VS. GASTELUMAugust 21st",
-    row2: "",
-  },
-  {
-    row1: "",
-    row2: "5Dimes",
-  },
-  {
-    row1: "Jared Cannonier",
-    row2: "",
-  },
-  {
-    row1: "Kelvin Gastelum",
-    row2: "",
-  },
-  {
-    row1: "Last change: 2 days ago",
-    row2: "",
-  },
-  {
-    row1: "UFC 266September 25th",
-    row2: "",
-  },
-  {
-    row1: "",
-    row2: "5Dimes",
-  },
-  {
-    row1: "Glover Teixeira",
-    row2: "190",
-  },
-  {
     row1: "Jan Blachowicz",
-    row2: "-250",
-  },
-  {
-    row1: "Johnny Walker",
-    row2: "",
-  },
-  {
-    row1: "Thiago Santos",
-    row2: "",
+    row2: "-260",
   },
 ];
-
 console.log("let odds = [");
 
 obj.forEach((row) => {
