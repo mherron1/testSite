c8b20aa7353c9cf8151e4a0ebd5cf09bb262f939
Michael Herron <mherron2021@gmail.com>
2021-08-25 13:29:58 +0100

    auto

--- scraper.js
+++ scraper.js
@@ -1,7 +1,6 @@
 const puppeteer = require("puppeteer");
 
 let events = [
-  "https://www.tapology.com/fightcenter/bouts/574111-paul-vs-woodley-jake-paul-vs-tyron-the-chosen-one-woodley",
   "https://www.tapology.com/fightcenter/events/79108-ufc-fight-night",
   "https://www.tapology.com/fightcenter/events/79654-ufc-fight-night",
   "https://www.tapology.com/fightcenter/events/79258-ufc-fight-night",
