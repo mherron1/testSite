04144b8d32a2010d8f9c919a68d2f4bca012cc55
Michael Herron <mherron2021@gmail.com>
2021-11-04 08:55:46 +0000

    update

---
+++ resultUpdate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+node shortScrape.js > tempInitial.js
+node resultsScraper.js > results.js
+node sample.js > database.js
+
+
+
+
+
+
--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 node scraper.js > initial.js
+node resultScraper.js > results.js
+node sample.js > database.js
 
 
 
