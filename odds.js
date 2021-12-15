939b7b6a47ccede33d83168c2bd11dfb3459c025
Michael Herron <mherron2021@gmail.com>
2021-12-15 23:24:13 +0000

    update

--- odds.js
+++ odds.js
@@ -1,162 +1,82 @@
 let odds = [
-{ name: 'Charles Oliveira', odds: '2.10' }
-,
-{ name: 'Dustin Poirier', odds: '1.77' }
-,
-{ name: 'Amanda Nunes', odds: '1.10' }
-,
-{ name: 'Julianna Pena', odds: '7.50' }
-,
-{ name: 'Geoff Neal', odds: '1.91' }
-,
-{ name: 'Santiago Ponzinibbio', odds: '1.91' }
-,
-{ name: 'Cody Garbrandt', odds: '1.77' }
-,
-{ name: 'Kai Kara France', odds: '2.10' }
-,
-{ name: 'Raulian Paiva', odds: '3.60' }
-,
-{ name: "Sean O'Malley", odds: '1.30' }
-,
-{ name: 'Dan Ige', odds: '2.25' }
-,
-{ name: 'Josh Emmett', odds: '1.69' }
-,
-{ name: 'Dominick Cruz', odds: '2.00' }
-,
-{ name: 'Pedro Munhoz', odds: '1.83' }
-,
-{ name: 'Augusto Sakai', odds: '2.00' }
-,
-{ name: 'Tai Tuivasa', odds: '1.83' }
-,
-{ name: 'Bruno Silva', odds: '1.27' }
-,
-{ name: 'Jordan Wright', odds: '3.80' }
-,
-{ name: 'Andre Muniz', odds: '1.61' }
-,
-{ name: 'Eryk Anders', odds: '2.45' }
-,
-{ name: 'Erin Blanchfield', odds: '2.20' }
-,
-{ name: 'Miranda Maverick', odds: '1.71' }
-,
-{ name: 'Darrick Minner', odds: '2.70' }
-,
-{ name: 'Ryan Hall', odds: '1.50' }
-,
-{ name: 'Randy Costa', odds: '1.59' }
-,
-{ name: 'Tony Kelley', odds: '2.50' }
-,
-{ name: 'Gillian Robertson', odds: '1.25' }
-,
-{ name: 'Priscila Cachoeira', odds: '4.00' }
-,
-{ name: 'ARES FCDecember 11th', odds: '1.11' }
-,
-{ name: 'Last change: 1 hr 28 min ago', odds: '1.27' }
-,
-{ name: 'CAGE WARRIORS 132December 11th', odds: '3.80' }
-,
-{ name: 'Chris Daukaus', odds: '1.71' }
-,
-{ name: 'Derrick Lewis', odds: '2.20' }
-,
-{ name: 'Belal Muhammad', odds: '3.00' }
-,
-{ name: 'Stephen Thompson', odds: '1.40' }
-,
-{ name: 'Cub Swanson', odds: '1.50' }
-,
-{ name: 'Darren Elkins', odds: '2.70' }
-,
-{ name: 'Amanda Lemos', odds: '1.24' }
-,
-{ name: 'Angela Hill', odds: '4.10' }
-,
-{ name: 'Carlos Diego Ferreira', odds: '2.55' }
-,
-{ name: 'Mateusz Gamrot', odds: '1.56' }
-,
-{ name: 'Raphael Assuncao', odds: '3.20' }
-,
-{ name: 'Ricky Simon', odds: '1.36' }
-,
-{ name: "Don'tale Mayes", odds: '1.50' }
-,
-{ name: 'Josh Parisian', odds: '2.70' }
-,
-{ name: 'Andre Ewell', odds: '2.70' }
-,
-{ name: 'Charles Jourdain', odds: '1.50' }
-,
-{ name: 'Melissa Gatto', odds: '2.80' }
-,
-{ name: 'Sijara Eubanks', odds: '1.45' }
-,
-{ name: 'Jordan Leavitt', odds: '1.91' }
-,
-{ name: 'Matt Sayles', odds: '1.91' }
-,
-{ name: 'Harry Hunsucker', odds: '3.60' }
-,
-{ name: 'Justin Tafa', odds: '1.30' }
-,
-{ name: 'Dustin Stoltzfus', odds: '2.85' }
-,
-{ name: 'Gerald Meerschaert', odds: '1.44' }
-,
-{ name: 'Macy Chiasson', odds: '2.55' }
-,
-{ name: 'Raquel Pennington', odds: '1.56' }
-,
-{ name: 'Last change: 3 hr 25 min ago', odds: '3.20' }
-,
-{ name: 'KSWDecember 18th', odds: '1.36' }
-,
-{ name: 'Last change: 14 hr 45 min ago', odds: '2.70' }
-,
-{ name: 'UFCDecember 31st', odds: '3.20' }
-,
-{ name: 'Last change: 34 days ago', odds: '1.95' }
-,
-{ name: 'TRILLER TRIAD COMBATDecember 31st', odds: '1.87' }
-,
-{ name: 'Last change: 9 days ago', odds: '1.30' }
-,
-{ name: 'Ciryl Gane', odds: '1.91' }
-,
-{ name: 'Francis Ngannou', odds: '1.91' }
-,
-{ name: 'Brandon Moreno', odds: '1.59' }
-,
-{ name: 'Deiveson Figueiredo', odds: '2.50' }
-,
-{ name: 'Ciryl Gane', odds: '1.91' }
-,
-{ name: 'Francis Ngannou', odds: '1.91' }
-,
-{ name: 'Brandon Moreno', odds: '1.59' }
-,
-{ name: 'Deiveson Figueiredo', odds: '2.50' }
-,
-{ name: 'Last change: 3 hr 29 min ago', odds: 'NaN' }
-,
-{ name: 'Last change: 24 days ago', odds: '2.88' }
-,
-{ name: 'Ciryl Gane', odds: '1.91' }
-,
-{ name: 'Francis Ngannou', odds: '1.91' }
-,
-{ name: 'Brandon Moreno', odds: '1.56' }
-,
-{ name: 'Deiveson Figueiredo', odds: '2.55' }
-,
-{ name: 'Last change: 10 hr 15 min ago', odds: '6.20' }
-,
-{ name: 'UFC FIGHT NIGHT 203February 26th', odds: '2.66' }
-,
-]
+  { name: "Charles Oliveira", odds: "2.10" },
+  { name: "Dustin Poirier", odds: "1.77" },
+  { name: "Amanda Nunes", odds: "1.10" },
+  { name: "Julianna Pena", odds: "7.50" },
+  { name: "Geoff Neal", odds: "1.91" },
+  { name: "Santiago Ponzinibbio", odds: "1.91" },
+  { name: "Cody Garbrandt", odds: "1.77" },
+  { name: "Kai Kara France", odds: "2.10" },
+  { name: "Raulian Paiva", odds: "3.60" },
+  { name: "Sean O'Malley", odds: "1.30" },
+  { name: "Dan Ige", odds: "2.25" },
+  { name: "Josh Emmett", odds: "1.69" },
+  { name: "Dominick Cruz", odds: "2.00" },
+  { name: "Pedro Munhoz", odds: "1.83" },
+  { name: "Augusto Sakai", odds: "2.00" },
+  { name: "Tai Tuivasa", odds: "1.83" },
+  { name: "Bruno Silva", odds: "1.27" },
+  { name: "Jordan Wright", odds: "3.80" },
+  { name: "Andre Muniz", odds: "1.61" },
+  { name: "Eryk Anders", odds: "2.45" },
+  { name: "Erin Blanchfield", odds: "2.20" },
+  { name: "Miranda Maverick", odds: "1.71" },
+  { name: "Darrick Minner", odds: "2.70" },
+  { name: "Ryan Hall", odds: "1.50" },
+  { name: "Randy Costa", odds: "1.59" },
+  { name: "Tony Kelley", odds: "2.50" },
+  { name: "Gillian Robertson", odds: "1.25" },
+  { name: "Priscila Cachoeira", odds: "4.00" },
+  { name: "ARES FCDecember 11th", odds: "1.11" },
+  { name: "Last change: 1 hr 28 min ago", odds: "1.27" },
+  { name: "CAGE WARRIORS 132December 11th", odds: "3.80" },
+  { name: "Chris Daukaus", odds: "1.71" },
+  { name: "Derrick Lewis", odds: "2.20" },
+  { name: "Belal Muhammad", odds: "3.00" },
+  { name: "Stephen Thompson", odds: "1.40" },
+  { name: "Cub Swanson", odds: "1.50" },
+  { name: "Darren Elkins", odds: "2.70" },
+  { name: "Amanda Lemos", odds: "1.24" },
+  { name: "Angela Hill", odds: "4.10" },
+  { name: "Diego Ferreira", odds: "2.55" },
+  { name: "Mateusz Gamrot", odds: "1.56" },
+  { name: "Raphael Assunção", odds: "3.20" },
+  { name: "Ricky Simón", odds: "1.36" },
+  { name: "Don'tale Mayes", odds: "1.50" },
+  { name: "Josh Parisian", odds: "2.70" },
+  { name: "Andre Ewell", odds: "2.70" },
+  { name: "Charles Jourdain", odds: "1.50" },
+  { name: "Melissa Gatto", odds: "2.80" },
+  { name: "Sijara Eubanks", odds: "1.45" },
+  { name: "Jordan Leavitt", odds: "1.91" },
+  { name: "Matt Sayles", odds: "1.91" },
+  { name: "Harry Hunsucker", odds: "3.60" },
+  { name: "Justin Tafa", odds: "1.30" },
+  { name: "Dustin Stoltzfus", odds: "2.85" },
+  { name: "Gerald Meerschaert", odds: "1.44" },
+  { name: "Macy Chiasson", odds: "2.55" },
+  { name: "Raquel Pennington", odds: "1.56" },
+  { name: "Last change: 3 hr 25 min ago", odds: "3.20" },
+  { name: "KSWDecember 18th", odds: "1.36" },
+  { name: "Last change: 14 hr 45 min ago", odds: "2.70" },
+  { name: "UFCDecember 31st", odds: "3.20" },
+  { name: "Last change: 34 days ago", odds: "1.95" },
+  { name: "TRILLER TRIAD COMBATDecember 31st", odds: "1.87" },
+  { name: "Last change: 9 days ago", odds: "1.30" },
+  { name: "Ciryl Gane", odds: "1.91" },
+  { name: "Francis Ngannou", odds: "1.91" },
+  { name: "Brandon Moreno", odds: "1.59" },
+  { name: "Deiveson Figueiredo", odds: "2.50" },
+  { name: "Ciryl Gane", odds: "1.91" },
+  { name: "Francis Ngannou", odds: "1.91" },
+  { name: "Brandon Moreno", odds: "1.59" },
+  { name: "Deiveson Figueiredo", odds: "2.50" },
+  { name: "Last change: 3 hr 29 min ago", odds: "NaN" },
+  { name: "Last change: 24 days ago", odds: "2.88" },
+  { name: "Ciryl Gane", odds: "1.91" },
+  { name: "Francis Ngannou", odds: "1.91" },
+  { name: "Brandon Moreno", odds: "1.56" },
+  { name: "Deiveson Figueiredo", odds: "2.55" },
+  { name: "Last change: 10 hr 15 min ago", odds: "6.20" },
+  { name: "UFC FIGHT NIGHT 203February 26th", odds: "2.66" },
+];
