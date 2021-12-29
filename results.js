b9fed162df070d1a1b55bb4ad6012fec23ee8fc4
Michael Herron <mherron2021@gmail.com>
2021-12-29 14:19:57 +0000

    update

--- results.js
+++ results.js
@@ -1,1 +1,255 @@
 let resultsList = [
+{
+  winner: 'Derrick Lewis ',
+  time: '3:36 Round 1',
+  method: 'ko/tko',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/25880-christopher-daukaus'
+}
+,
+{
+  winner: 'Belal Muhammad ',
+  time: '3 Rounds',
+  method: 'decision',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/16753-stephen-thompson-wonderboy'
+}
+,
+{
+  winner: 'Amanda Lemos ',
+  time: '3 Rounds',
+  method: 'decision',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/57525-angela-hill-overkill'
+}
+,
+{
+  winner: 'Ricky Simón ',
+  time: '2:14 Round 2',
+  method: 'ko/tko',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/raphael-assuncao'
+}
+,
+{
+  winner: 'Mateusz Gamrot ',
+  time: '3:26 Round 2',
+  method: 'ko/tko',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/carlos-ferreira'
+}
+,
+{
+  winner: 'Cub Swanson ',
+  time: '2:12 Round 1',
+  method: 'ko/tko',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/darren-elkins'
+}
+,
+{
+  winner: 'Gerald Meerschaert ',
+  time: '2:59 Round 3',
+  method: 'submission',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/34864-dustin-stoltzfus'
+}
+,
+{
+  winner: 'Justin Tafa ',
+  time: '1:54 Round 1',
+  method: 'ko/tko',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/24519-harry-hunsucker'
+}
+,
+{
+  winner: 'Melissa Gatto ',
+  time: '0:45 Round 3',
+  method: 'ko/tko',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/56532-sajara-eubanks'
+}
+,
+{
+  winner: 'Charles Jourdain ',
+  time: '3 Rounds',
+  method: 'decision',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/116569-andre-ewell'
+}
+,
+{
+  winner: 'Raquel Pennington ',
+  time: '3:07 Round 2',
+  method: 'submission',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/77737-macy-chiasson'
+}
+,
+{
+  winner: "Don'Tale Mayes ",
+  time: '3:26 Round 3',
+  method: 'ko/tko',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/86139-josh-parisian'
+}
+,
+{
+  winner: 'Jordan Leavitt ',
+  time: '2:05 Round 2',
+  method: 'submission',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/75678-matt-sayles-robo'
+}
+,
+{
+  winner: 'Derrick Lewis ',
+  time: '3:36 Round 1',
+  method: 'ko/tko',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/25880-christopher-daukaus'
+}
+,
+{
+  winner: 'Belal Muhammad ',
+  time: '3 Rounds',
+  method: 'decision',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/16753-stephen-thompson-wonderboy'
+}
+,
+{
+  winner: 'Amanda Lemos ',
+  time: '3 Rounds',
+  method: 'decision',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/57525-angela-hill-overkill'
+}
+,
+{
+  winner: 'Ricky Simón ',
+  time: '2:14 Round 2',
+  method: 'ko/tko',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/raphael-assuncao'
+}
+,
+{
+  winner: 'Mateusz Gamrot ',
+  time: '3:26 Round 2',
+  method: 'ko/tko',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/carlos-ferreira'
+}
+,
+{
+  winner: 'Cub Swanson ',
+  time: '2:12 Round 1',
+  method: 'ko/tko',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/darren-elkins'
+}
+,
+{
+  winner: 'Gerald Meerschaert ',
+  time: '2:59 Round 3',
+  method: 'submission',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/34864-dustin-stoltzfus'
+}
+,
+{
+  winner: 'Justin Tafa ',
+  time: '1:54 Round 1',
+  method: 'ko/tko',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/24519-harry-hunsucker'
+}
+,
+{
+  winner: 'Melissa Gatto ',
+  time: '0:45 Round 3',
+  method: 'ko/tko',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/56532-sajara-eubanks'
+}
+,
+{
+  winner: 'Charles Jourdain ',
+  time: '3 Rounds',
+  method: 'decision',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/116569-andre-ewell'
+}
+,
+{
+  winner: 'Raquel Pennington ',
+  time: '3:07 Round 2',
+  method: 'submission',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/77737-macy-chiasson'
+}
+,
+{
+  winner: "Don'Tale Mayes ",
+  time: '3:26 Round 3',
+  method: 'ko/tko',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/86139-josh-parisian'
+}
+,
+{
+  winner: 'Jordan Leavitt ',
+  time: '2:05 Round 2',
+  method: 'submission',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/75678-matt-sayles-robo'
+}
+,
+{
+  winner: 'Kayla Harrison ',
+  time: '4:00 Round 2',
+  method: 'submission',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/32612-taylor-stratford-no-mercy'
+}
+,
+{
+  winner: 'Ray Cooper III ',
+  time: '3:02 Round 3',
+  method: 'ko/tko',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/38087-magomed-magomedkerimov'
+}
+,
+{
+  winner: 'Abby Montes ',
+  time: '3 Rounds',
+  method: 'decision',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/200636-claressa-shields'
+}
+,
+{
+  winner: 'Bruno Cappelozza ',
+  time: '5 Rounds',
+  method: 'decision',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/43151-ante-delija-walking-trouble'
+}
+,
+{
+  winner: 'Movlid Khaybulaev ',
+  time: '5 Rounds',
+  method: 'decision',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/17954-chris-wade'
+}
+,
+{
+  winner: 'Antônio Carlos Júnior ',
+  time: '3:49 Round 1',
+  method: 'submission',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/144571-marthin-hamlet-nielsen'
+}
+,
+{
+  winner: 'Raush Manfio ',
+  time: '5 Rounds',
+  method: 'decision',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/95647-loik-razdhabov-jaguar-paw'
+}
+,
+{
+  winner: 'Jordan Young ',
+  time: '1:32 Round 3',
+  method: 'ko/tko',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/40111-omari-akhmedov'
+}
+,
+{
+  winner: 'Julia Budd ',
+  time: '3 Rounds',
+  method: 'decision',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/kaitlin-young'
+}
+,
+{
+  winner: 'Don Madge ',
+  time: '2:15 Round 2',
+  method: 'submission',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/67289-nathan-williams'
+}
+,
+]
+module.exports = resultsList;
