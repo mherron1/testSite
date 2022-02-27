a216c2302ab84b64693298b78cfd369ba19b5a4a
Michael Herron <mherron2021@gmail.com>
2022-02-27 01:57:52 +0000

    update

--- odds.js
+++ odds.js
@@ -2,7 +2,7 @@ let odds = [
 {"name":"Ji Yeon Kim" , "odds": "1.57"},
 {"name":"Priscila Cachoeira" , "odds": "2.35"},
 {"name":"Misha Cirkunov" , "odds": "1.40"},
-{"name":"Wellington Turman" , "odds": "6.50"},
+{"name":"Wellington Turman" , "odds": "6.00"},
 {"name":"Islam Makhachev" , "odds": "1.13"},
 {"name":"Bobby Green" , "odds": "8.20"},
 {"name":"Alex Oliveira" , "odds": "3.00"},
