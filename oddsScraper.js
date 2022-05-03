1936136b3fce6d3384605c41291dff31d3c4bcf4
Michael Herron <mherron2021@gmail.com>
2022-05-03 21:13:40 +0100

    update

--- oddsScraper.js
+++ oddsScraper.js
@@ -1,14 +1,5 @@
 const puppeteer = require("puppeteer");
 
-let events = [
-  "https://www.tapology.com/fightcenter/events/82805-ufc-fight-night",
-
-  "https://www.tapology.com/fightcenter/events/83245-bellator-mma",
-  "https://www.tapology.com/fightcenter/events/80968-pfl-2021-10-championships",
-  // "https://www.tapology.com/fightcenter/events/81070-one-championship-battleground-2",
-];
-//getEventURLs();
-
 let url = "https://www.oddschecker.com/ufc-mma";
 
 getCard(url);
