d1ec61d63ea3de12ea607ce0c1890d47ed165dcd
Michael Herron <mherron2021@gmail.com>
2022-07-07 14:48:49 +0100

    update

---
+++ ufcRankings.js
@@ -0,0 +1,888 @@
+let ufcRankings = [
+{
+            name: "Deiveson Figueiredo",
+            rank: 0,
+            index: 0
+            },
+{
+            name: "Brandon Moreno",
+            rank: 1,
+            index: 1
+            },
+{
+            name: "Kai Kara France",
+            rank: 2,
+            index: 2
+            },
+{
+            name: "Askar Askarov",
+            rank: 3,
+            index: 3
+            },
+{
+            name: "Alexandre Pantoja",
+            rank: 4,
+            index: 4
+            },
+{
+            name: "Brandon Royval",
+            rank: 5,
+            index: 5
+            },
+{
+            name: "Alex Perez",
+            rank: 6,
+            index: 6
+            },
+{
+            name: "Matheus Nicolau",
+            rank: 7,
+            index: 7
+            },
+{
+            name: "Matt Schnell",
+            rank: 8,
+            index: 8
+            },
+{
+            name: "David Dvorak",
+            rank: 9,
+            index: 9
+            },
+{
+            name: "Tim Elliott",
+            rank: 10,
+            index: 10
+            },
+{
+            name: "Sumudaerji",
+            rank: 11,
+            index: 11
+            },
+{
+            name: "Amir Albazi",
+            rank: 12,
+            index: 12
+            },
+{
+            name: "Manel Kape",
+            rank: 13,
+            index: 13
+            },
+{
+            name: "Tagir Ulanbekov",
+            rank: 14,
+            index: 14
+            },
+{
+            name: "Jeffrey Molina",
+            rank: 15,
+            index: 15
+            },
+{
+            name: "Aljamain Sterling",
+            rank: 0,
+            index: 16
+            },
+{
+            name: "Petr Yan",
+            rank: 1,
+            index: 17
+            },
+{
+            name: "TJ Dillashaw",
+            rank: 2,
+            index: 18
+            },
+{
+            name: "José Aldo",
+            rank: 3,
+            index: 19
+            },
+{
+            name: "Cory Sandhagen",
+            rank: 4,
+            index: 20
+            },
+{
+            name: "Marlon Vera",
+            rank: 5,
+            index: 21
+            },
+{
+            name: "Merab Dvalishvili",
+            rank: 6,
+            index: 22
+            },
+{
+            name: "Rob Font",
+            rank: 7,
+            index: 23
+            },
+{
+            name: "Dominick Cruz",
+            rank: 8,
+            index: 24
+            },
+{
+            name: "Pedro Munhoz",
+            rank: 9,
+            index: 25
+            },
+{
+            name: "Song Yadong",
+            rank: 10,
+            index: 26
+            },
+{
+            name: "Ricky Simon",
+            rank: 11,
+            index: 27
+            },
+{
+            name: "Frankie Edgar",
+            rank: 12,
+            index: 28
+            },
+{
+            name: "Sean O'Malley",
+            rank: 13,
+            index: 29
+            },
+{
+            name: "Jack Shore",
+            rank: 14,
+            index: 30
+            },
+{
+            name: "Umar Nurmagomedov",
+            rank: 15,
+            index: 31
+            },
+{
+            name: "Adrian Yanez",
+            rank: 0,
+            index: 32
+            },
+{
+            name: "Alexander Volkanovski",
+            rank: 1,
+            index: 33
+            },
+{
+            name: "Max Holloway",
+            rank: 2,
+            index: 34
+            },
+{
+            name: "Brian Ortega",
+            rank: 3,
+            index: 35
+            },
+{
+            name: "Josh Emmett",
+            rank: 4,
+            index: 36
+            },
+{
+            name: "Yair Rodriguez",
+            rank: 5,
+            index: 37
+            },
+{
+            name: "Calvin Kattar",
+            rank: 6,
+            index: 38
+            },
+{
+            name: "Arnold Allen",
+            rank: 7,
+            index: 39
+            },
+{
+            name: "Chan Sung Jung",
+            rank: 8,
+            index: 40
+            },
+{
+            name: "Giga Chikadze",
+            rank: 9,
+            index: 41
+            },
+{
+            name: "Bryce Mitchell",
+            rank: 10,
+            index: 42
+            },
+{
+            name: "Movsar Evloev",
+            rank: 11,
+            index: 43
+            },
+{
+            name: "Dan Ige",
+            rank: 12,
+            index: 44
+            },
+{
+            name: "Sodiq Yusuff",
+            rank: 13,
+            index: 45
+            },
+{
+            name: "Edson Barboza",
+            rank: 14,
+            index: 46
+            },
+{
+            name: "Shane Burgos",
+            rank: 15,
+            index: 47
+            },
+{
+            name: "Ilia Topuria",
+            rank: 0,
+            index: 48
+            },
+{
+            name: "Charles Oliveira",
+            rank: 1,
+            index: 49
+            },
+{
+            name: "Dustin Poirier",
+            rank: 2,
+            index: 50
+            },
+{
+            name: "Justin Gaethje",
+            rank: 3,
+            index: 51
+            },
+{
+            name: "Islam Makhachev",
+            rank: 4,
+            index: 52
+            },
+{
+            name: "Michael Chandler",
+            rank: 5,
+            index: 53
+            },
+{
+            name: "Beneil Dariush",
+            rank: 6,
+            index: 54
+            },
+{
+            name: "Rafael Dos Anjos",
+            rank: 7,
+            index: 55
+            },
+{
+            name: "Mateusz Gamrot",
+            rank: 8,
+            index: 56
+            },
+{
+            name: "Tony Ferguson",
+            rank: 9,
+            index: 57
+            },
+{
+            name: "Rafael Fiziev",
+            rank: 10,
+            index: 58
+            },
+{
+            name: "Arman Tsarukyan",
+            rank: 11,
+            index: 59
+            },
+{
+            name: "Conor McGregor",
+            rank: 12,
+            index: 60
+            },
+{
+            name: "Dan Hooker",
+            rank: 13,
+            index: 61
+            },
+{
+            name: "Jalin Turner",
+            rank: 14,
+            index: 62
+            },
+{
+            name: "null",
+            rank: 15,
+            index: 63
+            },
+{
+            name: "Damir Ismagulov",
+            rank: 0,
+            index: 64
+            },
+{
+            name: "Kamaru Usman",
+            rank: 1,
+            index: 65
+            },
+{
+            name: "Colby Covington",
+            rank: 2,
+            index: 66
+            },
+{
+            name: "Leon Edwards",
+            rank: 3,
+            index: 67
+            },
+{
+            name: "Khamzat Chimaev",
+            rank: 4,
+            index: 68
+            },
+{
+            name: "Gilbert Burns",
+            rank: 5,
+            index: 69
+            },
+{
+            name: "Belal Muhammad",
+            rank: 6,
+            index: 70
+            },
+{
+            name: "Vicente Luque",
+            rank: 7,
+            index: 71
+            },
+{
+            name: "Stephen Thompson",
+            rank: 8,
+            index: 72
+            },
+{
+            name: "Jorge Masvidal",
+            rank: 9,
+            index: 73
+            },
+{
+            name: "Sean Brady",
+            rank: 10,
+            index: 74
+            },
+{
+            name: "Shavkat Rakhmonov",
+            rank: 11,
+            index: 75
+            },
+{
+            name: "Michael Chiesa",
+            rank: 12,
+            index: 76
+            },
+{
+            name: "Neil Magny",
+            rank: 13,
+            index: 77
+            },
+{
+            name: "Geoff Neal",
+            rank: 14,
+            index: 78
+            },
+{
+            name: "Li Jingliang",
+            rank: 15,
+            index: 79
+            },
+{
+            name: "Michel Pereira",
+            rank: 0,
+            index: 80
+            },
+{
+            name: "Israel Adesanya",
+            rank: 1,
+            index: 81
+            },
+{
+            name: "Robert Whittaker",
+            rank: 2,
+            index: 82
+            },
+{
+            name: "Jared Cannonier",
+            rank: 3,
+            index: 83
+            },
+{
+            name: "Marvin Vettori",
+            rank: 4,
+            index: 84
+            },
+{
+            name: "Derek Brunson",
+            rank: 5,
+            index: 85
+            },
+{
+            name: "Paulo Costa",
+            rank: 6,
+            index: 86
+            },
+{
+            name: "Alex Pereira",
+            rank: 7,
+            index: 87
+            },
+{
+            name: "Sean Strickland",
+            rank: 8,
+            index: 88
+            },
+{
+            name: "Jack Hermansson",
+            rank: 9,
+            index: 89
+            },
+{
+            name: "Darren Till",
+            rank: 10,
+            index: 90
+            },
+{
+            name: "Andre Muniz",
+            rank: 11,
+            index: 91
+            },
+{
+            name: "Kelvin Gastelum",
+            rank: 12,
+            index: 92
+            },
+{
+            name: "Uriah Hall",
+            rank: 13,
+            index: 93
+            },
+{
+            name: "Nassourdine Imavov",
+            rank: 14,
+            index: 94
+            },
+{
+            name: "Dricus Du Plessis",
+            rank: 15,
+            index: 95
+            },
+{
+            name: "Brad Tavares",
+            rank: 0,
+            index: 96
+            },
+{
+            name: "Jiří Procházka",
+            rank: 1,
+            index: 97
+            },
+{
+            name: "Glover Teixeira",
+            rank: 2,
+            index: 98
+            },
+{
+            name: "Jan Błachowicz",
+            rank: 3,
+            index: 99
+            },
+{
+            name: "Aleksandar Rakić",
+            rank: 4,
+            index: 100
+            },
+{
+            name: "Magomed Ankalaev",
+            rank: 5,
+            index: 101
+            },
+{
+            name: "Anthony Smith",
+            rank: 6,
+            index: 102
+            },
+{
+            name: "Thiago Santos",
+            rank: 7,
+            index: 103
+            },
+{
+            name: "Dominick Reyes",
+            rank: 8,
+            index: 104
+            },
+{
+            name: "Paul Craig",
+            rank: 9,
+            index: 105
+            },
+{
+            name: "Volkan Oezdemir",
+            rank: 10,
+            index: 106
+            },
+{
+            name: "Jamahal Hill",
+            rank: 11,
+            index: 107
+            },
+{
+            name: "Nikita Krylov",
+            rank: 12,
+            index: 108
+            },
+{
+            name: "Ryan Spann",
+            rank: 13,
+            index: 109
+            },
+{
+            name: "Johnny Walker",
+            rank: 14,
+            index: 110
+            },
+{
+            name: "Jimmy Crute",
+            rank: 15,
+            index: 111
+            },
+{
+            name: "Dustin Jacoby",
+            rank: 0,
+            index: 112
+            },
+{
+            name: "Francis Ngannou",
+            rank: 1,
+            index: 113
+            },
+{
+            name: "Ciryl Gane",
+            rank: 2,
+            index: 114
+            },
+{
+            name: "Stipe Miocic",
+            rank: 3,
+            index: 115
+            },
+{
+            name: "Tai Tuivasa",
+            rank: 4,
+            index: 116
+            },
+{
+            name: "Curtis Blaydes",
+            rank: 5,
+            index: 117
+            },
+{
+            name: "Derrick Lewis",
+            rank: 6,
+            index: 118
+            },
+{
+            name: "Tom Aspinall",
+            rank: 7,
+            index: 119
+            },
+{
+            name: "Alexander Volkov",
+            rank: 8,
+            index: 120
+            },
+{
+            name: "Jairzinho Rozenstruik",
+            rank: 9,
+            index: 121
+            },
+{
+            name: "Chris Daukaus",
+            rank: 10,
+            index: 122
+            },
+{
+            name: "Marcin Tybura",
+            rank: 11,
+            index: 123
+            },
+{
+            name: "Sergei Pavlovich",
+            rank: 12,
+            index: 124
+            },
+{
+            name: "Shamil Abdurakhimov",
+            rank: 13,
+            index: 125
+            },
+{
+            name: "Alexandr Romanov",
+            rank: 14,
+            index: 126
+            },
+{
+            name: "Augusto Sakai",
+            rank: 15,
+            index: 127
+            },
+{
+            name: "Blagoy Ivanov",
+            rank: 0,
+            index: 128
+            },
+{
+            name: "Carla Esparza",
+            rank: 1,
+            index: 129
+            },
+{
+            name: "Rose Namajunas",
+            rank: 2,
+            index: 130
+            },
+{
+            name: "Zhang Weili",
+            rank: 3,
+            index: 131
+            },
+{
+            name: "Marina Rodriguez",
+            rank: 4,
+            index: 132
+            },
+{
+            name: "Mackenzie Dern",
+            rank: 5,
+            index: 133
+            },
+{
+            name: "Yan Xiaonan",
+            rank: 6,
+            index: 134
+            },
+{
+            name: "Jessica Andrade",
+            rank: 7,
+            index: 135
+            },
+{
+            name: "Nina Nunes",
+            rank: 8,
+            index: 136
+            },
+{
+            name: "Tecia Torres",
+            rank: 9,
+            index: 137
+            },
+{
+            name: "Amanda Ribas",
+            rank: 10,
+            index: 138
+            },
+{
+            name: "Michelle Waterson",
+            rank: 11,
+            index: 139
+            },
+{
+            name: "Amanda Lemos",
+            rank: 12,
+            index: 140
+            },
+{
+            name: "Virna Jandiroba",
+            rank: 13,
+            index: 141
+            },
+{
+            name: "Angela Hill",
+            rank: 14,
+            index: 142
+            },
+{
+            name: "Jessica Penne",
+            rank: 15,
+            index: 143
+            },
+{
+            name: "Luana Pinheiro",
+            rank: 0,
+            index: 144
+            },
+{
+            name: "Valentina Shevchenko",
+            rank: 1,
+            index: 145
+            },
+{
+            name: "Katlyn Chookagian",
+            rank: 2,
+            index: 146
+            },
+{
+            name: "Taila Santos",
+            rank: 3,
+            index: 147
+            },
+{
+            name: "Lauren Murphy",
+            rank: 4,
+            index: 148
+            },
+{
+            name: "Jessica Andrade",
+            rank: 5,
+            index: 149
+            },
+{
+            name: "Alexa Grasso",
+            rank: 6,
+            index: 150
+            },
+{
+            name: "Viviane Araujo",
+            rank: 7,
+            index: 151
+            },
+{
+            name: "Manon Fiorot",
+            rank: 8,
+            index: 152
+            },
+{
+            name: "Jennifer Maia",
+            rank: 9,
+            index: 153
+            },
+{
+            name: "Andrea Lee",
+            rank: 10,
+            index: 154
+            },
+{
+            name: "Maycee Barber",
+            rank: 11,
+            index: 155
+            },
+{
+            name: "Cynthia Calvillo",
+            rank: 12,
+            index: 156
+            },
+{
+            name: "Casey O'Neill",
+            rank: 13,
+            index: 157
+            },
+{
+            name: "Jessica Eye",
+            rank: 14,
+            index: 158
+            },
+{
+            name: "Erin Blanchfield",
+            rank: 15,
+            index: 159
+            },
+{
+            name: "Tracy Cortez",
+            rank: 0,
+            index: 160
+            },
+{
+            name: "Julianna Peña",
+            rank: 1,
+            index: 161
+            },
+{
+            name: "Amanda Nunes",
+            rank: 2,
+            index: 162
+            },
+{
+            name: "Ketlen Vieira",
+            rank: 3,
+            index: 163
+            },
+{
+            name: "Holly Holm",
+            rank: 4,
+            index: 164
+            },
+{
+            name: "Irene Aldana",
+            rank: 5,
+            index: 165
+            },
+{
+            name: "Raquel Pennington",
+            rank: 6,
+            index: 166
+            },
+{
+            name: "Yana Kunitskaya",
+            rank: 7,
+            index: 167
+            },
+{
+            name: "Aspen Ladd",
+            rank: 8,
+            index: 168
+            },
+{
+            name: "Sara McMann",
+            rank: 9,
+            index: 169
+            },
+{
+            name: "Pannie Kianzad",
+            rank: 10,
+            index: 170
+            },
+{
+            name: "Miesha Tate",
+            rank: 11,
+            index: 171
+            },
+{
+            name: "Macy Chiasson",
+            rank: 12,
+            index: 172
+            },
+{
+            name: "Lina Lansberg",
+            rank: 13,
+            index: 173
+            },
+{
+            name: "Karol Rosa",
+            rank: 14,
+            index: 174
+            },
+{
+            name: "Julia Avila",
+            rank: 15,
+            index: 175
+            },
+{
+            name: "Amanda Nunes",
+            rank: 0,
+            index: 176
+            },
+]
+module.exports = ufcRankings;
