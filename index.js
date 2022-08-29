329eb11916b56c8d36ad80d23dc8b00fd3019d97
Michael Herron <mherron2021@gmail.com>
2022-08-29 03:01:17 +0100

    update

--- index.js
+++ index.js
@@ -1106,7 +1106,7 @@ function generateCard(i, arg) {
         <a href="${events[i][3][j].fighterALink}" target="_blank">${events[i][3][j].fighterA}</a>
       </div>
       <div class="detailsLeft${arg}">
-      <div style="color: ${colorA}; font-size:0.9rem; font-weight:bold;">${leftOdds}</div>        
+      <div style="color: ${colorA}; font-size:0.8rem; font-weight:bold;">${leftOdds}</div>        
       <div >${events[i][3][j].rankA}</div>
         <div>${events[i][3][j].recordA}</div>
       </div>
@@ -1122,7 +1122,7 @@ function generateCard(i, arg) {
       <div class="detailsRight${arg}">
         <div>${events[i][3][j].recordB}</div>
         <div>${events[i][3][j].rankB}</div>
-        <div style="color: ${colorB}; font-size:0.9rem; font-weight:bold;">${rightOdds}</div>        
+        <div style="color: ${colorB}; font-size:0.8rem; font-weight:bold;">${rightOdds}</div>        
         </div>
     </div>
   
@@ -1268,7 +1268,7 @@ function generateCard(i, arg) {
           <a href="${events[i][3][j].fighterALink}" target="_blank" >${events[i][3][j].fighterA}</a>
         </div>
         <div class="detailsLeft${arg}">
-        <div style="color: ${colorA}; font-size:0.9rem; font-weight:bold;">${leftOdds}</div>        
+        <div style="color: ${colorA}; font-size:0.8rem; font-weight:bold;">${leftOdds}</div>        
         <div>${events[i][3][j].rankA}</div>
           <div>${events[i][3][j].recordA}</div>
         </div>
@@ -1284,7 +1284,7 @@ function generateCard(i, arg) {
         <div class="detailsRight${arg}">
           <div>${events[i][3][j].recordB}</div>
           <div>${events[i][3][j].rankB}</div>
-          <div style="color: ${colorB}; font-size:0.9rem; font-weight:bold;">${rightOdds}</div>        
+          <div style="color: ${colorB}; font-size:0.8rem; font-weight:bold;">${rightOdds}</div>        
           </div>
       </div>
       
