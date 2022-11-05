75b9f7a1c3a62a0392d10bbd7bb28ca2e801f27a
Michael Herron <mherron2021@gmail.com>
2022-11-05 19:08:36 +0000

    update

--- index.js
+++ index.js
@@ -736,9 +736,9 @@ function generateCard(i, arg) {
   <div id="broadcastInfo" ${broadcastStyle}> ${events[i][6]}</div>
   <br>
   <br>
-  <div style="text-align:center;">
+  <div style="text-align:center;" class="shareHolder">
 
-        <a style="text-decoration:underline; font-size:15px; color:#0000EE;" href="https://twitter.com/intent/tweet?url=https://mmafightcards.org/" target="_blank">
+        <a style="text-decoration:underline; font-size:15px;" href="https://twitter.com/intent/tweet?url=https://mmafightcards.org/" target="_blank">
           Share on Twitter
         </a>
 
