4c5d833cab84e209c7679c45b04941042d44521a
mherron1 <71177004+mherron1@users.noreply.github.com>
2023-06-11 13:36:47 +0200

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,8 +1,6 @@
 const puppeteer = require("puppeteer");
 
-let events = [
-  //upcoming cards
-
+const events = [
   "https://www.tapology.com/fightcenter/events/99416-ufc-294",
   "https://www.tapology.com/fightcenter/events/101315-ufc-293",
   "https://www.tapology.com/fightcenter/events/101389-ufc-fight-night",
@@ -17,232 +15,118 @@ let events = [
   "https://www.tapology.com/fightcenter/events/99780-ufc-fight-night",
   "https://www.tapology.com/fightcenter/events/99705-ufc-fight-night",
   "https://www.tapology.com/fightcenter/events/99346-ufc-289",
+  "https://www.tapology.com/fightcenter/events/99119-ufc-fight-night",
 ];
-//getEventURLs();
 
-console.log("let data = [");
+async function run() {
+  console.log("let data = [");
+  for (const event of events) {
+    await processEvent(event);
+  }
+  console.log("]");
+  console.log("module.exports = data;");
+}
 
-let index = 0;
-
-setTimeout(function () {
-  asyncGetCards();
-}, 0);
-
-function asyncGetCards() {
-  getCard(events[index]);
-  const delay = (t) => new Promise((resolve) => setTimeout(resolve, t));
-  delay(14000).then(() => {
-    index++;
-    if (index < events.length) {
-      asyncGetCards(index);
-    } else if ((index = events.length)) {
-      console.log("]");
-      console.log("module.exports = data;");
+async function processEvent(url) {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.setRequestInterception(true);
+  page.on("request", (req) => {
+    if (req.resourceType() === "stylesheet" || req.resourceType() === "image") {
+      req.abort();
+    } else {
+      req.continue();
     }
   });
-}
+  await page.goto(url);
 
-function getCard(url) {
-  (async () => {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.setRequestInterception(true);
-    page.on("request", (req) => {
-      if (
-        req.resourceType() == "stylesheet" ||
-        req.resourceType() === "image"
-      ) {
-        req.abort();
-      } else {
-        req.continue();
+  const eventTitle = await page.evaluate(() => {
+    const title = document.querySelector(".eventPageHeaderTitles h1").textContent.split(":")[0];
+    return title;
+  });
+
+  const eventDate = await page.evaluate(() => {
+    const eventDate = document.querySelector("#content > div.details.details_with_poster.clearfix > div.right > ul > li.header");
+    let dateArray = eventDate.textContent.split(" ");
+    let year = parseInt(dateArray[1].split(".")[2]);
+    let month = dateArray[1].split(".")[0] - 1;
+    let day = parseInt(dateArray[1].split(".")[1]);
+    let hour = parseInt(dateArray[3].split(":")[0]);
+
+    if (hour > 11) {
+      hour = hour + 5;
+    } else {
+      hour = hour + 12 + 5;
+      if (hour > 24) {
+        hour = hour - 24;
+        day += 1;
       }
-    });
-    await page.goto(url);
-
-    const eventTitle = await page.evaluate(() => {
-      const title = document
-        .querySelector(".eventPageHeaderTitles h1")
-        .textContent.split(":")[0];
-      return title;
-    });
-
-    const eventDate = await page.evaluate(() => {
-      const eventDate = document.querySelector(
-        "#content > div.details.details_with_poster.clearfix > div.right > ul > li.header"
-      );
-
-      let dateArray = eventDate.textContent.split(" "); ///remove textContent if origin scrape
-      let year = parseInt(dateArray[1].split(".")[2]);
-      let month = dateArray[1].split(".")[0] - 1;
-      let day = parseInt(dateArray[1].split(".")[1]);
-      let hour = parseInt(dateArray[3].split(":")[0]);
-
-      if (hour > 11) {
-        hour = hour + 5; ///////////// +5 hours to convert to UTC time
-      } else {
-        hour = hour + 12 + 5; ///////////// +5 hours to convert to UTC time
-        if (hour > 24) {
-          hour = hour - 24;
-          day += 1;
-        }
-      }
-      let minute = parseInt(dateArray[3].split(":")[1]);
-      let second = 0;
-      var main = new Date(year, month, day, hour, minute, second);
-
-      return Date.parse(main);
-    });
-
-    const promotion = await page.evaluate(() => {
-      const promotion = document
-        .querySelector("#content > div.details.details_with_poster.clearfix")
-        .getElementsByTagName("a")[0].textContent;
-      return promotion;
-    });
-
-    const poster = await page.evaluate(() => {
-      let posterSRC = document.querySelector(".left img").src;
-      return posterSRC;
-    });
-
-    const broadCast = await page.evaluate(() => {
-      let broadCast = document
-        .querySelector(
-          "#content > div.details.details_with_poster.clearfix > div.right > ul > li:nth-child(2)"
-        )
-        .innerText.split(" ")
-        .slice(2)
-        .join(" ");
-      return broadCast;
-    });
-
-    const fighterANames = await page.evaluate(() => {
-      let a = document.querySelectorAll(".fightCardFighterName.left");
-      const b = [...a];
-      return b.map((b) => b.innerText);
-    });
-    const fighterBNames = await page.evaluate(() => {
-      let a = document.querySelectorAll(".fightCardFighterName.right");
-      const b = [...a];
-      return b.map((b) => b.innerText);
-    });
-    const fightCardRecord = await page.evaluate(() => {
-      let a = document.querySelectorAll(".fightCardRecord");
-      const b = [...a];
-      return b.map((b) => b.innerText);
-    });
-    const fighterLeftRank = await page.evaluate(() => {
-      let a = document.querySelectorAll(".fightCardFighterRankNum.left");
-      const b = [...a];
-      return b.map((b) => b.innerText);
-    });
-    const fighterRightRank = await page.evaluate(() => {
-      let a = document.querySelectorAll(".fightCardFighterRankNum.right");
-      const b = [...a];
-      return b.map((b) => b.innerText);
-    });
-    const fighterWeights = await page.evaluate(() => {
-      let a = document.querySelectorAll(".weight");
-      const b = [...a];
-      return b.map((b) => b.innerText);
-    });
-    const fighterALink = await page.evaluate(() => {
-      let a = document.querySelectorAll(".fightCardFighterName.left");
-      const b = [...a];
-      return b.map((b) => b.firstElementChild.href);
-    });
-    const fighterBLink = await page.evaluate(() => {
-      let a = document.querySelectorAll(".fightCardFighterName.right");
-      const b = [...a];
-      return b.map((b) => b.firstElementChild.href);
-    });
-
-    const numPrelims = await page.evaluate(() => {
-      let fightsArr = document.querySelectorAll(".billing a");
-      let fArr = [...fightsArr];
-      let num = 0;
-      fArr.forEach((f) => {
-        if (f.textContent === "Prelim") {
-          num++;
-        }
-      });
-      return num;
-    });
-
-    a = fighterANames;
-    b = fighterBNames;
-    c = fightCardRecord;
-    d = fighterLeftRank;
-    e = fighterRightRank;
-    f = fighterWeights;
-    g = fighterALink;
-    h = fighterBLink;
-    t = eventDate;
-    p = poster;
-    bc = broadCast;
-    eventName = eventTitle;
-    promo = promotion;
-    n = numPrelims;
-    await browser.close();
-  })();
-
-  setTimeout(() => {
-    let fightCard = [];
-    let j = 0;
-    for (i = 0; i < a.length; i++) {
-      let k = j + 1;
-      let fighterA = a[i];
-      let fighterALink = g[i];
-
-      let rankA = "#n/a";
-      if (typeof d[i] === "undefined") {
-        e[i] = "#n/a";
-      } else {
-        rankA = d[i];
-      }
-
-      let recordA = c[j];
-      let fighterB = b[i];
-      let fighterBLink = h[i];
-
-      let rankB = "#n/a";
-      if (typeof e[i] === "undefined") {
-        e[i] = "#n/a";
-      } else {
-        rankB = e[i];
-      }
-
-      let recordB = c[k];
-      let weight = f[i];
-
-      /*rankA = "";
-      rankB = "";*/
-
-      let newBout = {
-        fighterA: fighterA,
-        fighterALink: fighterALink,
-        rankA: rankA,
-        recordA: recordA,
-        weight: weight,
-        recordB: recordB,
-        rankB: rankB,
-        fighterBLink: fighterBLink,
-        fighterB: fighterB,
-      };
-
-      fightCard.push(newBout);
-
-      k += 1;
-      j = k;
     }
+    let minute = parseInt(dateArray[3].split(":")[1]);
+    let second = 0;
+    return Date.parse(new Date(year, month, day, hour, minute, second));
+  });
 
-    console.log(`["${promo}",`);
-    console.log(`${t},`);
-    console.log("'',");
-    console.log(fightCard);
-    console.log(`,"${eventName}",`);
-    console.log(`"${n}",`);
-    console.log(`"${bc}"`);
-    console.log("],");
-  }, 13000);
+  const promotion = await page.evaluate(() => {
+    return document.querySelector("#content > div.details.details_with_poster.clearfix").getElementsByTagName("a")[0].textContent;
+  });
+
+  const poster = await page.evaluate(() => {
+    return document.querySelector(".left img").src;
+  });
+
+  const broadCast = await page.evaluate(() => {
+    return document.querySelector("#content > div.details.details_with_poster.clearfix > div.right > ul > li:nth-child(2)").innerText.split(" ").slice(2).join(" ");
+  });
+
+  const prelimCount = await page.evaluate(() => {
+    const fightsArr = document.querySelectorAll(".billing a");
+    let prelimCount = 0;
+    fightsArr.forEach((fight) => {
+      if (fight.textContent === "Prelim") {
+        prelimCount++;
+      }
+    });
+    return prelimCount;
+  });
+
+  const boutData = await page.evaluate(() => {
+    const boutElements = Array.from(document.querySelectorAll(".fightCard"));
+    return boutElements.map((boutElement) => {
+      const fighterA = boutElement.querySelector(".fightCardFighterName.left").innerText;
+      const fighterALink = boutElement.querySelector(".fightCardFighterName.left a").href;
+      const rankA = boutElement.querySelector(".fightCardFighterRankNum.left")?.innerText || "#n/a";
+      const recordA = boutElement.querySelector(".fightCardRecord").innerText;
+      const fighterB = boutElement.querySelector(".fightCardFighterName.right").innerText;
+      const fighterBLink = boutElement.querySelector(".fightCardFighterName.right a").href;
+      const rankB = boutElement.querySelector(".fightCardFighterRankNum.right")?.innerText || "#n/a";
+      const recordB = boutElement.nextElementSibling.querySelector(".fightCardRecord").innerText;
+      const weight = boutElement.nextElementSibling.querySelector(".weight").innerText;
+
+      return {
+        fighterA,
+        fighterALink,
+        rankA,
+        recordA,
+        fighterB,
+        fighterBLink,
+        rankB,
+        recordB,
+        weight,
+      };
+    });
+  });
+
+  console.log(`["${promotion}",`);
+  console.log(`${eventDate},`);
+  console.log("'',");
+  console.log(boutData);
+  console.log(`,"${eventTitle}",`);
+  console.log(`"${prelimCount}",`);
+  console.log(`"${broadCast}"`);
+  console.log("],");
+
+  await browser.close();
 }
+
+run();
