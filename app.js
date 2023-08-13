c581bb8404258db845a8787cd1ce15f2d0737203
Michael Herron <mherron2021@gmail.com>
2023-08-13 01:57:14 +0100

    update

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ let times = [
   {
     date: "aug12",
     promo: "Ultimate Fighting Championship",
-    time: new Date(2023, 07, 13, 02, 00, 00),
-    prelims: new Date(2023, 07, 12, 23, 00, 00),
+    time: new Date(2023, 07, 13, 00, 00, 00),
+    prelims: new Date(2023, 07, 12, 21, 00, 00),
     videos: [],
   },
 ];
