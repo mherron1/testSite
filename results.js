dbe4d28b022e55758d7bead3b3d4dd6efe2d67c7
Michael Herron <mherron2021@gmail.com>
2023-09-15 23:58:29 +0100

    update

--- results.js
+++ results.js
@@ -1,1 +1,87 @@
 let resultsList = [
+{
+  winner: 'Sean Strickland ',
+  time: '5 Rounds',
+  method: 'decision',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/79163-israel-adesanya'
+}
+,
+{
+  winner: 'Alexander Volkov ',
+  time: '4:37 Round 2',
+  method: 'submission',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/120884-tai-tuivasa-bam-bam'
+}
+,
+{
+  winner: 'Manel Kape ',
+  time: '3 Rounds',
+  method: 'decision',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/247470-felipe-dos-santos-lipe-detona'
+}
+,
+{
+  winner: 'Justin Tafa ',
+  time: '1:22 Round 1',
+  method: 'ko/tko',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/131141-austen-lane'
+}
+,
+{
+  winner: 'Tyson Pedro ',
+  time: '2:12 Round 1',
+  method: 'ko/tko',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/115015-anton-turkalj'
+}
+,
+{
+  winner: 'Carlos Ulberg ',
+  time: '4:49 Round 3',
+  method: 'submission',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/103262-da-un-jung'
+}
+,
+{
+  winner: 'Chepe Mariscal ',
+  time: '3:19 Round 2',
+  method: 'ko/tko',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/134851-jack-jenkins'
+}
+,
+{
+  winner: 'Jamie Mullarkey ',
+  time: '3 Rounds',
+  method: 'decision',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/john-makdessi-the-bull'
+}
+,
+{
+  winner: 'Nasrat Haqparast ',
+  time: '3 Rounds',
+  method: 'decision',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/124140-landon-quinones'
+}
+,
+{
+  winner: 'Charles Radtke ',
+  time: '3 Rounds',
+  method: 'decision',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/166857-mike-diamond-blood'
+}
+,
+{
+  winner: 'Gabriel Miranda ',
+  time: '0:59 Round 1',
+  method: 'submission',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/61870-shane-young'
+}
+,
+{
+  winner: 'Kevin Jousset ',
+  time: '4:49 Round 1',
+  method: 'submission',
+  b_Link: 'https://www.tapology.com/fightcenter/fighters/69153-kiefer-crosbie'
+}
+,
+]
+module.exports = resultsList;
